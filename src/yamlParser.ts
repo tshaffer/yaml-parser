57676195767125b9302c4e178b42f02e194b36cf
Ted Shaffer <ted@brightsign.biz>
2019-06-13 14:52:46 -0700

    further progress - parsing yaml in typescript

--- src/yamlParser.ts
+++ src/yamlParser.ts
@@ -1,3 +1,10 @@
+import * as fs from 'fs-extra';
+import { safeLoad } from 'js-yaml';
+
 export function parseYaml() {
   console.log('parseYaml invoked in typescript file');
+
+  const bmapYamlData: any = safeLoad(fs.readFileSync('BMAP.yaml', 'utf8'));
+  console.log(bmapYamlData);
+  
 }
